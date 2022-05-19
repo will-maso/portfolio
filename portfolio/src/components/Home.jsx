98ffe0ec755d27992550f522519c883c2bcc7b99
will-maso <ww=m4705211@gmail.com>
2022-05-19 20:58:06 +0100

    slight changes

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -2,10 +2,22 @@ const Home = () => {
     return <div className="Home">
         <h2>Portfolio:</h2>
         <ul className="List">
-            <li><h4>NC news frontend project:</h4>This app allows you to view news articles. The list of articles is intially populated with all the articles in the db nicely paginated for ease of access.<br></br>Articles can then be sorted using a variety of parameters in the drop down menu. Comments can be viewed on each article and comments can be added by existing users,<br></br> as well as upvotes being added by only existing users.<br></br><a href="https://github.com/will-maso/nc-news" className="Link">GitHub repository</a> / <a href="https://super-cool-site-by-will-maso.netlify.app/" className="Link">Hosted Frontend</a></li>
-            <li><h4>NC news backend project:</h4>This project is a backend project that houses a database containing articles, users, comments and topics tables <br></br>
-                each of which is interlinked and provides multiple endpoints from which you can extract useful information.<br></br><a href="https://github.com/will-maso/be-news" className="Link">GitHub repository</a> / <a href="https://news-williammason.herokuapp.com/" className="Link">Hosted Database</a></li>
-            <li><h4>DnDinder fullstack project:</h4></li>
+            <li><h4>NC news frontend project:</h4>
+            This app allows you to view news articles. The list of articles is intially populated with all the articles in the db nicely paginated for ease of access.
+            Articles can then be sorted using a variety of parameters in the drop down menu. Comments can be viewed on each article and comments can be added by existing users,
+            as well as upvotes being added by only existing users.<br></br>
+            <a href="https://github.com/will-maso/nc-news" className="Link">GitHub repository</a> / <a href="https://super-cool-site-by-will-maso.netlify.app/" className="Link">Hosted Frontend</a></li>
+            <li><h4>NC news backend project:</h4>
+            This project is a backend project that houses a database containing articles, users, comments and topics tables 
+            each of which is interlinked and provides multiple endpoints from which you can extract useful information. <br></br>
+            <a href="https://github.com/will-maso/be-news" className="Link">GitHub repository</a> / <a href="https://news-williammason.herokuapp.com/" className="Link">Hosted Database</a></li>
+            <li><h4>DnDinder fullstack project:</h4>
+            This project is a meeting place for DnD players, it allows them to create a user profile, 
+            they are also able to create groups and add other users to their group. On the group page the dungeon master's email will be displayed to allow them to organise a game, 
+            we plan to add a chat functionality in the future. All of this is done using firebase aswell as cloud functions to make a serverless backend, 
+            while the frontend uses React.<br></br>
+            <a href="https://github.com/boa-constructor/backend-firebase" className="Link">Backend repository</a> / <a href="https://github.com/boa-constructor/frontend-react-app" className="Link">Frontend repository</a>
+            </li>
         </ul>
         </div>
 }
--- portfolio/src/components/NavBar.jsx
+++ portfolio/src/components/NavBar.jsx
@@ -6,8 +6,8 @@ const NavBar = () => {
         <Link to="/Contact" className="Link"> Contact </Link> /
         <Link to="/About" className="Link"> About </Link>
         <br></br>
-        <Link to="/GitHub" target="_blank" className="Link" ><img src="https://github.githubassets.com/images/modules/logos_page/Octocat.png" alt="GitHub logo" width="40" height="40"></img>  </Link>
-        <Link to="/LinkedIn" target="_blank" className="Link" ><img src="https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg" alt="LinkedIn logo" width="40" height="40"></img></Link>
+        <a target="_blank" className="Link" href="https://github.com/will-maso"><img src="https://github.githubassets.com/images/modules/logos_page/Octocat.png" alt="GitHub logo" width="40" height="40"></img>  </a>
+        <a target="_blank" className="Link" href="https://www.linkedin.com/in/william-mason-73aa1b239/"><img src="https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg" alt="LinkedIn logo" width="40" height="40"></img></a>
     </nav>
 }
 
