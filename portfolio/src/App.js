d3847bab02667cc22613bfd147dfd7fc696fcc3e
will-maso <ww=m4705211@gmail.com>
2022-06-02 14:53:37 +0100

    major changes to portfolio

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -5,34 +5,97 @@ import Contact from './components/Contact';
 import About from './components/About';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useState } from 'react';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import spaceImg from './space.jpg';
+import willImg from './will.jpg';
 
 function App() {
   const [darkmode, setDarkMode] = useState(false);
-
-  const toggleTheme = () => {
-    setDarkMode((currDarkmode) => {
-      return !currDarkmode;
-    });
-  };
-
-  return (
-    <BrowserRouter>
-      <div className={`App__${darkmode ? 'Light' : 'Dark'}`}>
-        <NavBar />
-        <button
-          onClick={() => toggleTheme()}
-          className={`button__${darkmode ? 'Dark' : 'Light'}`}
-        >
-          {darkmode ? 'Dark' : 'Light'}mode
-        </button>
-        <Routes>
-          <Route path='/' element={<Home />}></Route>
-          <Route path='/Contact' element={<Contact />}></Route>
-          <Route path='/About' element={<About />}></Route>
-        </Routes>
-      </div>
-    </BrowserRouter>
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
   );
+  const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('#bg'),
+  });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+  const material = new THREE.MeshStandardMaterial({
+    color: 0xff6347,
+    wireframe: true,
+  });
+  const torus = new THREE.Mesh(geometry, material);
+  scene.add(torus);
+  const pointLight = new THREE.PointLight(0xffffff);
+  pointLight.position.set(20, 20, 20);
+  const ambientLight = new THREE.AmbientLight(0xffffff);
+  scene.add(pointLight, ambientLight);
+  const controls = new OrbitControls(camera, renderer.domElement);
+
+  function addStar() {
+    const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+    const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+    const star = new THREE.Mesh(geometry, material);
+    const [x, y, z] = Array(3)
+      .fill()
+      .map(() => THREE.MathUtils.randFloatSpread(100));
+    star.position.set(x, y, z);
+    scene.add(star);
+  }
+
+  Array(200).fill().forEach(addStar);
+
+  const spaceTexture = new THREE.TextureLoader().load(spaceImg);
+  scene.background = spaceTexture;
+
+  function moveCamera() {
+    const t = document.body.getBoundingClientRect().top;
+
+    camera.position.z = t * -0.01;
+    camera.position.x = t * -0.0002;
+    camera.rotation.y = t * -0.0002;
+  }
+
+  document.body.onscroll = moveCamera;
+  function animate() {
+    requestAnimationFrame(animate);
+    torus.rotation.x += 0.01;
+    torus.rotation.y += 0.005;
+    torus.rotation.z += 0.01;
+    controls.update();
+    renderer.render(scene, camera);
+  }
+  animate();
+  // const toggleTheme = () => {
+  //   setDarkMode((currDarkmode) => {
+  //     return !currDarkmode;
+  //   });
+  // };
+
+  // return (
+  //   <BrowserRouter>
+  //     {/* <div className={`App__${darkmode ? 'Light' : 'Dark'}`}>
+  //       <NavBar />
+  //       <button
+  //         onClick={() => toggleTheme()}
+  //         className={`button__${darkmode ? 'Dark' : 'Light'}`}
+  //       >
+  //         {darkmode ? 'Dark' : 'Light'}mode
+  //       </button>
+  //       <Routes>
+  //         <Route path='/' element={<Home />}></Route>
+  //         <Route path='/Contact' element={<Contact />}></Route>
+  //         <Route path='/About' element={<About />}></Route>
+  //       </Routes>
+  //     </div> */}
+  //   </BrowserRouter>
+  // );
 }
 
 export default App;
--- portfolio/src/components/NavBar.jsx
+++ portfolio/src/components/NavBar.jsx
@@ -4,7 +4,8 @@ const NavBar = () => {
     return <nav>
         <Link to="/" className="Link">William Mason</Link> /
         <Link to="/Contact" className="Link"> Contact </Link> /
-        <Link to="/About" className="Link"> About </Link>
+        <Link to="/About" className="Link"> About </Link> / 
+        <a target="_blank" className="Link" href="https://docs.google.com/document/d/16JTFwP8kwiaxliBRb7Sf_iE60Z9y1KQITpkeyJsMOgQ/edit?usp=sharing" rel="noreferrer"> CV</a>
         <br></br>
         <a target="_blank" className="Link" href="https://github.com/will-maso" rel="noreferrer"><img src="https://github.githubassets.com/images/modules/logos_page/Octocat.png" alt="GitHub logo" width="40" height="40"></img>  </a>
         <a target="_blank" className="Link" href="https://www.linkedin.com/in/william-mason-73aa1b239/" rel="noreferrer"><img src="https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg" alt="LinkedIn logo" width="40" height="40"></img></a>
